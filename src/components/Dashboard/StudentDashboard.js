42fe3216732acd71c7debedf446993ae91f8933a
Babatunde Lawrence Afolabi <88771845+Mclawrenceco@users.noreply.github.com>
2024-07-01 19:23:40 +0100

    Create StudentDashboard.js

---
+++ src/components/Dashboard/StudentDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StudentDashboard = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">Student Dashboard</h1>
+            {/* Add dashboard content... */}
+        </div>
+    );
+};
+
+export default StudentDashboard;
