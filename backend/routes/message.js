51e309f0b5a7d1107d61ec848a6247c3974a08e5
Babatunde Lawrence Afolabi <88771845+Mclawrenceco@users.noreply.github.com>
2024-07-01 19:01:24 +0100

    Create message.js

---
+++ backend/routes/message.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { sendMessage, getMessages } = require('../controllers/messageController');
+const router = express.Router();
+
+router.post('/', sendMessage);
+router.get('/:userId', getMessages);
+
+module.exports = router;
